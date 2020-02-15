fd7e7b8f01feafadf80fd7e4c2060aa58f86b543
Kolkos <a.vanderkolk@kolkos.nl>
2020-02-15 08:26:10 +0100

    first draft

--- src/main/java/nl/kolkos/crypto/telegram/bot/backend/entities/Coin.java
+++ src/main/java/nl/kolkos/crypto/telegram/bot/backend/entities/Coin.java
@@ -1,5 +1,31 @@
 package nl.kolkos.crypto.telegram.bot.backend.entities;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Builder(toBuilder = true)
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Setter(value = AccessLevel.PACKAGE)
+@Getter
 public class Coin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String symbol;
+
+
 }
